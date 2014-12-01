43007e0ab3e99490ea68001694961e6bca8c9289
Jared Deckard <jared.deckard@gmail.com>
2014-12-01 12:54:20 -0800

    Remove WS and Cloudflare configuration

--- deployment.config.js
+++ deployment.config.js
@@ -1,31 +1,15 @@
 var grunt = require('grunt');
 
-var awsCredentials = grunt.file.readJSON('aws.credentials.json');
-var cloudflareCredentials = grunt.file.readJSON('cloudflare.credentials.json');
 var environments = grunt.file.readJSON('deployment.environments.json');
 
 var envCfg = function(environment) {
-  console.log("Using environment " + environment + " for AWS deployment, if invoked.");
+  console.log("Using environment " + environment + " for deployment, if invoked.");
   return environments[environment];
 }
 
 module.exports = function(env) {
   var config = envCfg(env);
   return {
-    aws: {
-      accessKeyId: awsCredentials.accessKeyId,
-      secretAccessKey: awsCredentials.secretAccessKey,
-    },
-    s3: {
-      bucket: config.bucket,
-      enableWeb: true
-    },
-    cloudflare: {
-      email: cloudflareCredentials.email,
-      api_key: cloudflareCredentials.api_key,
-      domain: config.cloudflare_domain
-    },
-    
     api         : config.api         ? config.api : "",
     mixpanel    : config.mixpanel    ? config.mixpanel : "",
     ga_account  : config.ga_account  ? config.ga_account : "",
