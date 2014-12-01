ee0bcb3ac46a04192d29d3c57fe30b380e580a4f
Jared Deckard <jared.deckard@gmail.com>
2014-12-01 14:13:26 -0800

    Include public config files

---
+++ src/config.js
@@ -0,0 +1,21 @@
+/* config vars must be global */
+
+Options = {
+  theme     : "light",
+  base      : {currency:"BTC", issuer:"gDSSa75HPagWcvQmwH7D51dT5DPmvsKL4q"},
+  trade     : {currency:"STR", issuer:""},
+  chartType : "line",
+  interval  : "1h",
+ 
+  stellar    : {
+    
+    trace   : false,
+    trusted : false,
+
+    servers: [
+      { host: 'live.stellar.org', port: 9001, secure: true }
+    ],
+
+    connection_offset: 0
+  }
+}
